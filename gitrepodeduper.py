9a2397340e5cd6a12204ae62034d5318bd278fd8
Matthias Krüger <matthias.krueger@famsik.de>
2016-07-13 19:15:45 +0200

    implement cross repo deduping. Fixes #1.
    You can run
    gitrepodeduper <path/to/repo1> <../../path/to/repo2> </path/to/repo3>
    to make it take hashes of all these repos into account

--- gitrepodeduper.py
+++ gitrepodeduper.py
@@ -18,7 +18,7 @@
 #    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA  02110-1301 USA
 
 
-# USAGE:  gitrepodeduper  | duperemove -dh -
+# USAGE:  gitrepodeduper <repo1> <repo2> <...> | duperemove -dh -
 
 import sys
 if (sys.version_info.major != 3): # no python 3
@@ -26,22 +26,33 @@ if (sys.version_info.major != 3): # no python 3
 	sys.exit(1)
 
 import subprocess
+import os
 
+original_dir = os.getcwd() # dir where we started the script
+repos = sys.argv # copy list
+repos.pop(0) # remove first entry which is the script name
+if (not repos[0]):
+	repos[0] = os.getcwd()
 
-reporoot = subprocess.Popen(["git", "rev-parse", "--show-toplevel"], stdout=subprocess.PIPE).communicate()[0].decode("utf-8").split("\n")[0]
+set(repos) # remove dupes
 
-# shell:
-# git ls-files  -s  | cut -d' ' -f2 | sort -n | uniq -c | sort -n 
-rawhashlist = subprocess.Popen(["git", "ls-files", "-s"], stdout=subprocess.PIPE).communicate()[0].decode("utf-8").replace('\t',' ').split("\n")
+hashlist = []
+for directory in repos:
+	os.chdir(directory)
+	reporoot = subprocess.Popen(["git", "rev-parse", "--show-toplevel"], stdout=subprocess.PIPE).communicate()[0].decode("utf-8").split("\n")[0]
 
-hashlist= []
-for index, line in enumerate(rawhashlist): # list into sublist with words
+	# shell:
+	# git ls-files  -s  | cut -d' ' -f2 | sort -n | uniq -c | sort -n 
+	rawhashlist = subprocess.Popen(["git", "ls-files", "-s"], stdout=subprocess.PIPE).communicate()[0].decode("utf-8").replace('\t',' ').split("\n")
 
-	if line == "":
-		continue
+	for index, line in enumerate(rawhashlist): # list into sublist with words
+		if line == "":
+			continue
 
-	splitline = line.split()
-	hashlist.append([splitline[1],reporoot + "/" + splitline[3]]) #new list [hash, filename]
+		splitline = line.split()
+		hashlist.append([splitline[1],reporoot + "/" + splitline[3]]) #new list [hash, filename]
+
+	os.chdir(original_dir) # we need to go back to the original dir to have relative paths still work
 
 hashlist.sort() #sort by hash
 
